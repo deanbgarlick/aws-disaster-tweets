5e7249e01f6fb05b42e20e96a607fb7f7e7301a8
deanbgarlick <deanbgarlick@gmail.com>
2021-11-22 22:46:22 -0800

    Update / route response

--- inference/src/app.py
+++ inference/src/app.py
@@ -34,7 +34,7 @@ def index():
     app_name = os.getenv("APP_NAME")
 
     if app_name:
-        return f"Hello from {app_name} running in a Docker container behind Nginx!"
+        return f"Hello from {app_name}!"
 
     return "Hello from Flask"
 
